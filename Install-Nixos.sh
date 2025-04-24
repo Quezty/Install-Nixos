5e2b90f37ff6c59c23953f6863e24335cdb2ac6d
Joachim Storli <122435731+Quezty@users.noreply.github.com>
2025-04-24 21:45:12 +0000

    small change in command order, and added check for nixos configuration requirements that is required for the script to work

--- Install-Nixos.sh
+++ Install-Nixos.sh
@@ -10,7 +10,26 @@ main() {
 
     echo -e "\n \n \n"
 
+
+    line=$(nix config show | grep "experimental-features")
+
+    if echo "$line"	| grep -qw flakes && echo "$line" | grep -qw nix-command; then
+	    echo "Experimental features enabled"
+    else 
+	    echo "Experimental features needed for the script to work was not enabled add the following lines to your configuration for the script to work"
+        echo -e '- nix.settings.experimental-features = [ "nix-command" "flakes" ]; \n \n'
+    fi
+
     is_git_installed=$(dpkg -l  | grep git)
+
+    if [[ -z "$is_git_installed" ]]; then
+        echo "git is not installed, install git and try again"
+    else 
+        echo "git is installed, pulling from repository"
+    fi
+
+    git clone https://github.com/Quezty/NixosConfiguration.git tmp-files/NixosConfiguration
+
 
     if [ -d "$HOME/nixos" ]; then
         echo "Directory exists"
@@ -19,14 +38,6 @@ main() {
         mkdir ~/nixos
         echo "nixos folder created in home directory of $USER"
     fi
-
-    if [[ -z "$is_git_installed" ]]; then
-        echo "git is not installed, install git and try again"
-    else 
-        echo "git is installed, pulling from repository"
-    fi
-
-   git clone https://github.com/Quezty/NixosConfiguration.git tmp-files/NixosConfiguration
 }
 
 main "$@"
